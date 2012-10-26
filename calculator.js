246692c4b2d5c89538b0dd8d4a6234482f46ca58
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2012-10-26 15:32:04 +0530

    Simple operations without validattions

--- calculator.js
+++ calculator.js
@@ -1,13 +1,63 @@
 var buttons = document.getElementsByTagName('button');
-buttons[0].onclick=alert("clicked 0");
+var dataStack =[];
+var textbox = document.getElementById('text');
+var isEqualPressed = false;
+var clearField = false;
 
-function display() {
-	alert("clicked");
+var display = function(result){
+	document.getElementById('text').value = "" + result;
 }
 
-for(button in buttons){
-	button.onclick = display;
+var execute = function(operator) {
+	clearField = true;
+	if(dataStack.length == 0) {
+		var operand=parseFloat(textbox.value);
+		dataStack.push(operand);
+		dataStack.push(operator);
+		return;
+	}
+	var oldOp = dataStack.pop();
+	var operand1 = parseFloat(dataStack.pop());
+	var operand2 = parseFloat(textbox.value);
+	var result = operation(operand1, oldOp, operand2);
+	display(result);
+	if(operator == "=" ) {
+		isEqualPressed = true;	
+		return;
+	}
+	dataStack.push(result);
+	dataStack.push(operator);
 }
 
+var operation = function(operand1, oldOp, operand2) {
+	switch(oldOp){
+		case "+":
+			return operand1 + operand2;
+			break;
+		case "-":
+			return operand1 - operand2;
+			break;
+		case "*":
+		    return operand1 * operand2;
+			break;
+		case "/":
+			return operand1 / operand2;
+			break;
+	}
+}
 
+var calculate = function () {
+	if(!isNaN(this.value)) {
+		if(clearField) {
+			textbox.value = "";
+			clearField = false;
+		}
+		textbox.value += this.value;
+		return;
+	}
+	execute(this.value);	
+}
 
+for(var i=0; i<buttons.length; i++) {
+	buttons[i].onclick = calculate;
+}
