86facc03de416762d6a35b1718f050cf16ff4cb9
Vishnu Narang <vishnun@thoughtworks.com>
2012-10-29 10:44:09 +0530

    Implemented scoping of all functions and variables

--- calculator.js
+++ calculator.js
@@ -1,120 +1,127 @@
-var buttons = document.getElementsByTagName('button');
-var dataStack =[];
-var textbox = document.getElementById('text');
-var clearField = false;
-var DECIMAL = '.',
-	memory = 0;
+var calculator = function () {
 
-var push = function(value, operator) {
-	dataStack.push(value);
-	dataStack.push(operator);
-}
+	var buttons = document.getElementsByTagName('button'),
+		dataStack =[],
+		textbox = document.getElementById('text'),
+		clearField = false,
+		DECIMAL = '.',
+		memory = 0,
 
-var display = function(data) {
-	if(data > parseInt(data)) {
-		data = data.toFixed(2);
-	}
-	if(data.toString().length > 10)
-		data = data.toExponential(5);
-	textbox.value = data;
-}
+	push = function(value, operator) {
+		dataStack.push(value);
+		dataStack.push(operator);
+	},
 
-var isInvalid = function(oldOp, operand2) {
-	if(oldOp == '/' && operand2 == 0)
-		return true;
-	return false;
-}
+	display = function(data) {
+		if(data > parseInt(data)) {
+			data = data.toFixed(2);
+		}
+		if(data.toString().length > 10)
+			data = data.toExponential(5);
+		textbox.value = data;
+	},
 
-var execute = function(operator) {
-	clearField = true;
-	if(dataStack.length > 0) {
-		var oldOp = dataStack.pop();
-		var operand1 = parseFloat(dataStack.pop());
-		var operand2 = parseFloat(textbox.value);
-		if(isInvalid(oldOp, operand2)) {
-			textbox.value = "E";
+	isInvalid = function(oldOp, operand2) {
+		if(oldOp == '/' && operand2 == 0)
+			return true;
+		return false;
+	},
+
+	execute = function(operator) {
+		clearField = true;
+		if(dataStack.length > 0) {
+			var oldOp = dataStack.pop(),
+				operand1 = parseFloat(dataStack.pop()),
+				operand2 = parseFloat(textbox.value);
+
+			if(isInvalid(oldOp, operand2)) {
+				textbox.value = "E";
+				return;
+			}
+			var result = operation(operand1, oldOp, operand2);
+			display(result);
+			if(operator!= '='){			
+				push(result, operator);
+			}		
 			return;
 		}
-		console.log("text: " +textbox.value);
-		console.log(operand1 + " " + operand2);
-		var result = operation(operand1, oldOp, operand2);
-		display(result);
-		if(operator!= '='){			
-			push(result, operator);
-		}		
-		return;
-	}
-	if(operator == '=')
-		return;
-	if(textbox.value == "")
-		textbox.value = "0";
-	push(textbox.value, operator);
-}
-
-var operation = function(operand1, oldOp, operand2) {
-	switch(oldOp) {
-		case "+": return operand1 + operand2;
-		case "-": return operand1 - operand2;
-		case "*": return operand1 * operand2;
-		case "/": return operand1 / operand2;
-	}
-}
-
-var isOperator = function(input) {
-	return input == '+' || input == '-' ||input == '*' 
-					||input == '/' || input == '=' ;
-}
-
-var memoryOperator = function (mOperator) {
-	return mOperator == 'MR' || mOperator == 'M+' || mOperator == 'M-';
-}
-
-var mExecute = function(mOperator) {
-	if(mOperator == 'M+'){
-		memory += parseFloat(textbox.value);
-	}
-	else if(mOperator == 'M-'){
-	 	memory -= parseFloat(textbox.value);
-	}
-	else {
-		textbox.value = parseFloat(memory);
-	}
-}
-
-var calculate = function () {
-
-	if(memoryOperator(this.value)){
-		mExecute(this.value);	
-		return;	
-	}
-	if(isOperator(this.value)) {
-		execute(this.value);	
-		return;
-	}
-	if(clearField) {
-		textbox.value = "";
-		clearField = false;
-	}
-	if(!isNaN(this.value)) {
-		if(textbox.value.length == 10)
+		if(operator == '=')
 			return;
-		textbox.value += this.value;
-		return;
-	}
-	if(this.value == DECIMAL) {
-		if(textbox.value == "") {
+		if(textbox.value == "")
 			textbox.value = "0";
+		push(textbox.value, operator);
+	},
+
+	operation = function(operand1, oldOp, operand2) {
+		switch(oldOp) {
+			case "+": return operand1 + operand2;
+			case "-": return operand1 - operand2;
+			case "*": return operand1 * operand2;
+			case "/": return operand1 / operand2;
 		}
-		textbox.value += this.value;
-	}
-	if(this.value == "C") {
-		textbox.value = "";
-		memory = 0;
-		dataStack = [];
-	}
+	},
 
-}
+	isOperator = function(input) {
+		return input == '+' || input == '-' ||input == '*' 
+						||input == '/' || input == '=' ;
+	},
 
-for(var i=0; i<buttons.length; i++) {
-	buttons[i].onclick = calculate;
-}
+	memoryOperator = function (mOperator) {
+		return mOperator == 'MR' || mOperator == 'M+' || mOperator == 'M-';
+	},
+
+	mExecute = function(mOperator) {
+		if(mOperator == 'M+'){
+			memory += parseFloat(textbox.value);
+		}
+		else if(mOperator == 'M-'){
+		 	memory -= parseFloat(textbox.value);
+		}
+		else {
+			textbox.value = parseFloat(memory);
+		}
+	},
+
+	calculate = function () {
+
+		if(memoryOperator(this.value)){
+			mExecute(this.value);	
+			return;	
+		}
+		if(isOperator(this.value)) {
+			execute(this.value);	
+			return;
+		}
+		if(clearField) {
+			textbox.value = "";
+			clearField = false;
+		}
+		if(!isNaN(this.value)) {
+			if(textbox.value.length == 10)
+				return;
+			textbox.value += this.value;
+			return;
+		}
+		if(this.value == DECIMAL) {
+			if(textbox.value == "") {
+				textbox.value = "0";
+			}
+			textbox.value += this.value;
+		}
+		if(this.value == "C") {
+			textbox.value = "";
+			memory = 0;
+			dataStack = [];
+		}
+
+	};
+
+	var init = function(){
+		for(var i=0; i<buttons.length; i++) {
+			buttons[i].onclick = calculate;
+		}
+	};
+	return init();
+};
+
+document.onload = calculator();
